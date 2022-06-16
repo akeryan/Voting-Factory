c5dcde0884b0bfd946b8212f8d6759997ba79040
Aram Keryan <aram.keryan@gmail.com>
2022-06-16 19:15:39 +0400

    Update VotingFactory.sol

--- contracts/VotingFactory.sol
+++ contracts/VotingFactory.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VotingFactory is Ownable {
+contract VotingFactory {
 
     struct Candidate {
 		string	name; // candidate name
@@ -19,34 +19,43 @@ contract VotingFactory is Ownable {
         mapping (address => uint) isCandidate; //candidateAddr => candidateID
         address[] voters; // list of all the voters
 		mapping (address => uint) isVoter; // voterAddr => candidateID             
-        uint startDate;
+        uint startDate; // specifies when campaign opens for voting
         uint payableBalance; // funds payable to the winner  
-        bool isFinalized;        	            			    
+        bool isFinalized; // turns true when voting is closed       	            			    
     }
 
+	address public owner;
+
     uint private _votingCounter; // to get subsequent voting ID;
-    uint[] private _votingIDs; // list of voting campaign ID's
-    mapping(uint => Voting) public _votings; // votingID => Voting
+    uint[] public votingIDs; // list of voting campaign ID's
+    mapping(uint => Voting) public votings; // votingID => Voting
 
     uint private _totalReceivedFunds; // total funds received from all campaigns
     uint private _releasedCommission; // total commission retrieved by the owner    
 
 
-    modifier votingExists(uint votingID) {
-        require(_votings[votingID].startDate > 0, "Voting doesn't exist");
+    modifier onlyOwner {
+		require(msg.sender == owner, "Not an owner");
+		_;
+	}
+	
+	modifier votingExists(uint votingID) {
+        require(votings[votingID].startDate > 0, "Voting doesn't exist");
         _;
     }
 
     // use only after modifier "votingExists"
     modifier candidateExists(uint votingID, uint candidateID) {
         require(
-			candidateID > 0 && candidateID <= _votings[votingID].numCandidates,
+			candidateID > 0 && candidateID <= votings[votingID].numCandidates,
             "Wrong candidateID"
         );
         _;
     }
 
-	//constructor () { }
+	constructor () {
+		owner = msg.sender; 
+	}
 
     function newVoting(
         string[] calldata names, 
@@ -55,7 +64,7 @@ contract VotingFactory is Ownable {
     ) 
         external
         onlyOwner 
-        returns (uint votingID) 
+        returns (uint votingID_) 
     {
         require(names.length > 1, "Must be > 1 candidates");
         require(names.length == addresses.length, 
@@ -65,10 +74,10 @@ contract VotingFactory is Ownable {
 		require(startDate_ < block.timestamp + 15778463,
 			"Cannot schedule beyond 6 months period");
 
-        votingID = _votingCounter++;
-        Voting storage v = _votings[votingID];
-        v.index = _votingIDs.length;
-        _votingIDs.push() = votingID;
+        votingID_ = _votingCounter++;
+        Voting storage v = votings[votingID_];
+        v.index = votingIDs.length;
+        votingIDs.push() = votingID_;
 
         v.startDate = startDate_;
         
@@ -81,12 +90,12 @@ contract VotingFactory is Ownable {
         onlyOwner
         votingExists(votingID) 
     {
-		require(_votings[votingID].winnerID != 0, 
+		require(votings[votingID].isFinalized == true, 
 			"Can delete only after the voting is closed"
 		);
-        Voting storage v = _votings[votingID];
+        Voting storage v = votings[votingID];
 
-        for(uint i; i < v.voters.length; i++) {
+        for(uint i = 0; i < v.voters.length; i++) {
             delete v.isVoter[v.voters[i]];
         }
 
@@ -95,15 +104,15 @@ contract VotingFactory is Ownable {
             delete v.candidates[i];            
         }
 
-        uint index_ = _votings[votingID].index;
+        uint index_ = votings[votingID].index;
 
-        if(index_ != _votingIDs.length - 1) {
-            _votingIDs[index_] = _votingIDs[_votingIDs.length - 1];
-            _votings[_votingIDs[index_]].index = index_;
+        if(index_ != votingIDs.length - 1) {
+            votingIDs[index_] = votingIDs[votingIDs.length - 1];
+            votings[votingIDs[index_]].index = index_;
         }
 
-        _votingIDs.pop();
-        delete _votings[votingID];
+        votingIDs.pop();
+        delete votings[votingID];
     }
 
     function vote(uint votingID, uint candidateID) 
@@ -112,13 +121,13 @@ contract VotingFactory is Ownable {
         votingExists(votingID) 
         candidateExists(votingID, candidateID)        
     {
-        require(block.timestamp >= _votings[votingID].startDate, 
+        require(block.timestamp >= votings[votingID].startDate, 
             "Voting not started"
         );
-        require(block.timestamp <= _votings[votingID].startDate + 3 days,
+        require(block.timestamp <= votings[votingID].startDate + 3 days,
             "Voting ended"
         );
-        Voting storage v = _votings[votingID];
+        Voting storage v = votings[votingID];
         require(v.isCandidate[msg.sender] == 0, "Candidates cannot vote");
         require(v.isVoter[msg.sender] == 0, "You've already voted");        
         require(msg.value == 1 * 10**16, "Must be 0.01 ETH");
@@ -134,10 +143,11 @@ contract VotingFactory is Ownable {
     function closeVoting(uint votingID) 
         external
         votingExists(votingID) 
+		returns (uint)
     {
-        Voting storage v = _votings[votingID];
+        Voting storage v = votings[votingID];
         require(v.isFinalized == false, "Already finalized");
-        require(block.timestamp > _votings[votingID].startDate + 3 days,
+        require(block.timestamp > votings[votingID].startDate + 3 days,
             "Only after voting end date"
         );
 
@@ -145,14 +155,15 @@ contract VotingFactory is Ownable {
 			v.winnerID = _winner(votingID);
 			payable(v.candidates[v.winnerID].addr).transfer(v.payableBalance);
 		} 
-        v.isFinalized = true;       
+        v.isFinalized = true;    
+		return v.winnerID;   
     }
 
     function withdrawCommission() external onlyOwner {
         uint balance = _commissionBalance();
         require(balance > 0, "No available balance");
         _releasedCommission = _releasedCommission + balance;
-        payable(owner()).transfer(balance);        
+        payable(owner).transfer(balance);        
     } 
 
     function totalFundsRecieved() 
@@ -173,25 +184,6 @@ contract VotingFactory is Ownable {
         return _releasedCommission;
     }
 
-    function isCandidate(uint votingId, address addr) 
-        external 
-        view 
-        votingExists(votingId)
-        returns (uint) 
-    {
-        return _votings[votingId].isCandidate[addr];
-    }
-
-    function hasVoted(uint votingID, address addr) 
-        external 
-        view 
-        votingExists(votingID)
-        returns (bool out) 
-    {
-        if(_votings[votingID].isVoter[addr] > 0)
-            out = true;
-    }
-
     function numVotes(uint votingID, uint candidateID) 
         external 
         view
@@ -199,16 +191,16 @@ contract VotingFactory is Ownable {
         candidateExists(votingID, candidateID)
         returns (uint) 
     {
-        return _votings[votingID].candidates[candidateID].numVotes;
+        return votings[votingID].candidates[candidateID].numVotes;
     }
 
     function startDate(uint votingID) 
-    external 
+    public 
     view
     votingExists(votingID) 
     returns (uint) 
     {
-        return _votings[votingID].startDate;
+        return votings[votingID].startDate;
     }
 
     function endDate(uint votingID) 
@@ -217,20 +209,11 @@ contract VotingFactory is Ownable {
     votingExists(votingID) 
     returns (uint) 
     {
-        return _votings[votingID].startDate + 3 days;
+        return startDate(votingID) + 3 days;
     }  
 
     function commissionBalance() external view onlyOwner returns(uint) {
         return _commissionBalance();
-    }
-
-    function numCandidates(uint votingID) 
-        external 
-        view 
-        votingExists(votingID) 
-        returns (uint) 
-    {
-        return _votings[votingID].numCandidates;
     }
 
     function numVoters(uint votingID) 
@@ -239,7 +222,7 @@ contract VotingFactory is Ownable {
     votingExists(votingID) 
     returns (uint) 
     {
-        return _votings[votingID].voters.length;
+        return votings[votingID].voters.length;
     } 
 
     function candidate(uint votingID, uint candidateID) 
@@ -250,28 +233,21 @@ contract VotingFactory is Ownable {
         returns (string memory, address, uint) 
     {
         return (
-            _votings[votingID].candidates[candidateID].name,
-            _votings[votingID].candidates[candidateID].addr,
-			_votings[votingID].candidates[candidateID].numVotes
+            votings[votingID].candidates[candidateID].name,
+            votings[votingID].candidates[candidateID].addr,
+			votings[votingID].candidates[candidateID].numVotes
         );
     }
 
-	function winnerInfo(uint votingID) 
-        external 
-        view 
-        votingExists(votingID) 
-        returns (Candidate memory) 
-    {
-        Voting storage v = _votings[votingID];
-        require(v.isFinalized == true, "Must be finalized first");
-        require(v.winnerID > 0, "Not identified yet");
-		
-        return v.candidates[v.winnerID];
-    }
-
-    function rewardAmount(uint votingID) external view returns (uint) {
-        return _votings[votingID].payableBalance;
-    }
+	function votingIndex(uint votingID) 
+		public 
+		view
+		onlyOwner
+		votingExists(votingID) 
+		returns (uint) 
+	{
+		return votings[votingID].index;
+	}
 
     function _addCandidate(Voting storage v, string memory name_, address addr_) 
 		private 
@@ -293,8 +269,8 @@ contract VotingFactory is Ownable {
     function _commissionBalance() private view returns (uint) {
         uint payableFunds;
 
-        for(uint i; i < _votingIDs.length; i++) {
-            payableFunds = payableFunds + _votings[i].payableBalance;
+        for(uint i; i < votingIDs.length; i++) {
+            payableFunds = payableFunds + votings[i].payableBalance;
         }
 
         return (_totalReceivedFunds - payableFunds - _releasedCommission);
@@ -305,10 +281,11 @@ contract VotingFactory is Ownable {
         view
         returns (uint) 
     {
-        Voting storage v = _votings[votingID];
+        Voting storage v = votings[votingID];
 
         uint[] memory tempWinners = new uint[](v.numCandidates);
         uint winningCount = v.candidates[1].numVotes;
+		tempWinners[0] = 1;
         uint numWinners = 1;  
 
         for(uint i = 2; i <= v.numCandidates; i++)
@@ -323,8 +300,8 @@ contract VotingFactory is Ownable {
 
         if(numWinners > 1)
             return tempWinners[_randNumber(numWinners)];
-        else
-            return tempWinners[0];
+        
+		return tempWinners[0];
     }
 
     // returns a random number in range [0; maxValue)
@@ -333,9 +310,5 @@ contract VotingFactory is Ownable {
             uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp))) 
             % maxValue
         );
-    }
-
-    function contractBalance() external view returns (uint) {
-        return address(this).balance;
     }
 }
